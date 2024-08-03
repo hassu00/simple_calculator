b3dbd817198de35df7cab6c7b3b7bb3fafd86be7
hassu00 <rgbeasty@gmail.com>
2024-08-03 14:16:24 +0500

    push my calculater in github

---
+++ index.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    { name: "q1", message: "what`s your name", type: "input" },
+    { name: "q2", message: "write first number", type: "input" },
+    { name: "q3", message: "write secound number", type: "input" },
+    { name: "q4", message: "choose an operator", type: "list", choices: ["addition", "subtraction", "division", "multiplication"] }
+]);
+let result = 0;
+if (answer.q4 === "addition") {
+    console.log(result = answer.q2 + answer.q3);
+}
+else if (answer.q4 === "subtraction") {
+    console.log(result = answer.q2 - answer.q3);
+}
+else if (answer.q4 === "division") {
+    console.log(result = answer.q2 / answer.q3);
+}
+else if (answer.q4 === "multiplication") {
+    console.log(result = answer.q2 * answer.q3);
+}
+else {
+    console.log("please select a valid operater");
+}
+console.log("your answer is", result);
